891ff147e9d07b5c1174d732effad135f5d47fda
aydanbrown <aydan@evident.co.nz>
2017-03-28 08:39:26 +1300

    Added verify-article.c

---
+++ verify-article/verify-article.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+
+#define element_count 3
+
+char *elements[element_count] = {
+	"itemprop=\"contentUrl\"",
+	"itemprop=\"articleBody\"",
+	"itemprop=\"datePublished\""
+};
+
+int check[element_count];
+
+FILE *file;
+
+char c;
+
+void next()
+{
+	c = fgetc(file);
+}
+
+int match()
+{
+	file = fopen("page.html", "r");
+
+	next();
+
+	while(!feof(file))
+	{
+		int matched = 0;
+		for(int i = 0; i < element_count; i++)
+		{
+			if(check[i] != -1)
+			{
+				if(elements[i][check[i]] == 0)
+				{
+					check[i] = -1;
+					printf("Matched: %s\n", elements[i]);
+					matched++;
+				}
+				else if(elements[i][check[i]] == c)
+				{
+					check[i]++;
+				}
+				else
+				{
+					check[i] = 0;
+				}
+			}
+			else
+			{
+				matched++;
+			}
+		}
+
+		if(matched == element_count) { return 0; }
+
+		next();
+	}
+
+	return 1;
+}
+
+int main(void)
+{
+	if(match() == 0)
+	{
+		printf("Verified\n");
+	}
+	else
+	{
+		printf("Invalid\n");
+	}
+
+	return 0;
+}
+
